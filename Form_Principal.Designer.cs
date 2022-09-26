393cf1ad9084189dad8ece21d3b8c503a5d3f0b4
FelipeChaparim <felipechaparim08@gmail.com>
2022-09-26 16:03:59 -0300

    Commit

---
+++ Form_Principal.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+namespace ToDoList
+{
+    partial class Form_Principal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Txb_Novo = new System.Windows.Forms.TextBox();
+            this.Btn_Add = new System.Windows.Forms.Button();
+            this.Flp_Concluido = new System.Windows.Forms.FlowLayoutPanel();
+            this.Flp_Afazer = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // Txb_Novo
+            // 
+            this.Txb_Novo.Location = new System.Drawing.Point(12, 12);
+            this.Txb_Novo.Multiline = true;
+            this.Txb_Novo.Name = "Txb_Novo";
+            this.Txb_Novo.Size = new System.Drawing.Size(259, 59);
+            this.Txb_Novo.TabIndex = 0;
+            // 
+            // Btn_Add
+            // 
+            this.Btn_Add.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Btn_Add.Location = new System.Drawing.Point(277, 26);
+            this.Btn_Add.Name = "Btn_Add";
+            this.Btn_Add.Size = new System.Drawing.Size(90, 32);
+            this.Btn_Add.TabIndex = 1;
+            this.Btn_Add.Text = "Adicionar";
+            this.Btn_Add.UseVisualStyleBackColor = true;
+            this.Btn_Add.Click += new System.EventHandler(this.Btn_Add_Click);
+            this.Btn_Add.MouseEnter += new System.EventHandler(this.MouseEnter);
+            this.Btn_Add.MouseLeave += new System.EventHandler(this.MouseLeave);
+            // 
+            // Flp_Concluido
+            // 
+            this.Flp_Concluido.AutoScroll = true;
+            this.Flp_Concluido.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Flp_Concluido.Location = new System.Drawing.Point(314, 96);
+            this.Flp_Concluido.Name = "Flp_Concluido";
+            this.Flp_Concluido.Size = new System.Drawing.Size(290, 535);
+            this.Flp_Concluido.TabIndex = 3;
+            // 
+            // Flp_Afazer
+            // 
+            this.Flp_Afazer.AutoScroll = true;
+            this.Flp_Afazer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Flp_Afazer.Location = new System.Drawing.Point(12, 96);
+            this.Flp_Afazer.Name = "Flp_Afazer";
+            this.Flp_Afazer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Flp_Afazer.Size = new System.Drawing.Size(290, 535);
+            this.Flp_Afazer.TabIndex = 4;
+            // 
+            // Form_Principal
+            // 
+            this.AcceptButton = this.Btn_Add;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(616, 643);
+            this.Controls.Add(this.Flp_Afazer);
+            this.Controls.Add(this.Flp_Concluido);
+            this.Controls.Add(this.Btn_Add);
+            this.Controls.Add(this.Txb_Novo);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form_Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Txb_Novo;
+        private System.Windows.Forms.Button Btn_Add;
+        private System.Windows.Forms.FlowLayoutPanel Flp_Concluido;
+        private System.Windows.Forms.FlowLayoutPanel Flp_Afazer;
+    }
+}
+
---
+++ Form_Principal.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form_Principal : Form
+    {
+        private int Cbx_Fazer_Count = 0;
+        private int Cbx_Concluido_Count = 0;
+        private int Pnl_Fazer_Count = 0;
+        private int Pnl_Concluido_Count = 0;
+        private int Btn_Delete_count = 0;
+
+        private readonly Button[] Bc = new Button[999];
+        private readonly CheckBox[] Cf = new CheckBox[999];
+        private readonly Panel[] Nf = new Panel[999];
+        private readonly CheckBox[] Cc = new CheckBox[999];
+        private readonly Panel[] Nc = new Panel[999];
+
+        public Form_Principal()
+        {
+            InitializeComponent();
+        }
+        private void Btn_Add_Click(object sender, EventArgs e)
+        {
+            Nf[Pnl_Fazer_Count] = new Panel
+            {
+                Location = new Point(10, 10),
+                Size = new Size(282, 28)
+            };
+            Flp_Afazer.Controls.Add(Nf[Pnl_Fazer_Count]);
+
+            Cf[Cbx_Fazer_Count] = new CheckBox
+            {
+                Location = new Point(4, 4),
+                Size = new Size(),
+                Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0),
+                AutoSize = true,
+                Text = Txb_Novo.Text
+            };
+            Cf[Cbx_Fazer_Count].CheckedChanged += new EventHandler(Cbx_Acao);
+
+            Nf[Pnl_Fazer_Count].Controls.Add(Cf[Cbx_Fazer_Count]);
+
+            Cf[Cbx_Fazer_Count].MouseEnter += new EventHandler(MouseEnter);
+            Cf[Cbx_Fazer_Count].MouseLeave += new EventHandler(MouseLeave);
+
+            Cbx_Fazer_Count++;
+            Pnl_Fazer_Count++;
+            Txb_Novo.Text = "";
+
+            Txb_Novo.Focus();
+        }
+        private void Cbx_Acao(object sender, EventArgs e)
+        {
+            for (int x = 0; x < Cbx_Fazer_Count; x++)
+            {
+                if (Cf[x].Checked == true)
+                {
+                    Nc[Pnl_Concluido_Count] = new Panel
+                    {
+                        Location = new Point(10, 10),
+                        Size = new Size(282, 28)
+                    };
+                    Flp_Concluido.Controls.Add(Nc[Pnl_Concluido_Count]);
+
+                    Cc[Cbx_Concluido_Count] = new CheckBox
+                    {
+                        Location = new Point(4, 8),
+                        Size = new Size(),
+                        Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Strikeout, GraphicsUnit.Point, 0),
+                        AutoSize = true,
+                        Text = Cf[x].Text,
+                        Checked = true
+                    };
+
+                    Bc[Btn_Delete_count] = new Button
+                    {
+                        Location = new Point(235, 4),
+                        Font = new Font("Microsoft Sans Serif", 8.00F, FontStyle.Underline, GraphicsUnit.Point, 0),
+                        ForeColor = Color.Red,
+                        Text = "X",
+                        UseVisualStyleBackColor = true,
+                        Size = new Size(23, 23),
+                        FlatStyle = FlatStyle.Flat,
+                        DialogResult = DialogResult.OK
+                    };
+                    Bc[Btn_Delete_count].Click += new EventHandler(Deleta);
+                    Bc[Btn_Delete_count].MouseEnter += new EventHandler(MouseEnter);
+                    Bc[Btn_Delete_count].MouseLeave += new EventHandler(MouseLeave);
+
+                    Cc[Cbx_Concluido_Count].MouseEnter += new EventHandler(MouseEnter);
+                    Cc[Cbx_Concluido_Count].MouseLeave += new EventHandler(MouseLeave);
+
+                    Nc[Pnl_Concluido_Count].Controls.Add(Cc[Cbx_Concluido_Count]);
+                    Nc[Pnl_Concluido_Count].Controls.Add(Bc[Btn_Delete_count]);
+
+                    Cbx_Concluido_Count++;
+                    Pnl_Concluido_Count++;
+                    Btn_Delete_count++;
+
+                    Organiza(x);
+                    break;
+                }
+            }
+        }
+        private void Organiza(int ind)
+        {
+            int x;
+            for (x = ind; x < Cbx_Fazer_Count - 1; x++)
+            {
+                Cf[x].Text = Cf[x + 1].Text;
+                Cf[x].Checked = false;
+            }
+
+            Nf[Pnl_Fazer_Count - 1].Controls.Remove(Cf[Cbx_Fazer_Count - 1]);
+            Flp_Afazer.Controls.Remove(Nf[Pnl_Fazer_Count - 1]);
+
+            Nf[x] = null;
+            Cf[x] = null;
+
+            Pnl_Fazer_Count--;
+            Cbx_Fazer_Count--;
+        }
+        private void Deleta(object sender, EventArgs e)
+        {
+            
+        }
+        private new void MouseEnter(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Hand;
+        }
+
+        private new void MouseLeave(object sender, EventArgs e)
+        {
+            Cursor = Cursors.Default;
+        }
+    }
+}
